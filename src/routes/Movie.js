d873c14a2291935f48bc67a8533f367532b28549
Shin Hyunjae <shyunjan@naver.com>
2022-10-05 21:35:08 +0900

    complete the work for MovieDetail component

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -4,9 +4,10 @@ import MovieInfo from '../components/MovieInfo';
 const Movie = () => {
   const [loading, setLoading] = useState(true);
   const [movieData, setMovieData] = useState({});
-
   const getMovies = async () => {
-    const jsonResponse = await (await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')).json();
+    const jsonResponse = await (
+      await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
+    ).json();
     // console.log(jsonResponse.data);
     setMovieData(jsonResponse.data);
     setLoading(false);
