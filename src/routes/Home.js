aac1e53da2ccf6963d1d16db5a034ce08cd00771
Shin Hyunjae <shyunjan@naver.com>
2022-10-06 21:33:35 +0900

    add a comment for the contributor to the Home component, fix CSS in Movie component and Deploy done

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,13 @@ export default function Home() {
       <div style={{ textAlign: 'center' }}>
         <h1>ReactJS Sample Code Project</h1>
         <h3>Select a sample page from the left menu</h3>
+        <br />
+        <h4>
+          These codes were contributed by{' '}
+          <a href="https://nomadcoders.co/" target={'_blank'} rel="noreferrer">
+            Nomad_Coders(https://nomadcoders.co/)
+          </a>
+        </h4>
       </div>
     </div>
   );
